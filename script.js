52c1501e589f74cd585b736b07ccfdf8e3aaaf74
fortunat81 <163460178+fortunat81@users.noreply.github.com>
2024-07-27 12:56:35 -0400

    create function to play one round

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ console.log("hello world");
 
 let choices = ["rock", "paper", "scissors"];
 
+let humanScore = 0;
+let computerScore = 0;
+
+let round = 0;
+
 function getComputerChoice(){
     return choices[Math.floor(Math.random() * 3)];
 }
@@ -9,7 +14,31 @@ function getComputerChoice(){
 function getHumanChoice(){
     let choice = "";
     while(choice != "rock" && choice != "paper" && choice != "scissors"){
-        choice = prompt("rock, paper, scissors").toLowerCase();
+        choice = prompt("rock, paper, scissors");
+        choice = choice.toLowerCase();
     }
     return choice;
 }
+
+function playRound(computerChoice, humanChoice){
+    let tieText = `${humanChoice} is the same as ${computerChoice} it's a tie`;
+    let winText = `${humanChoice} beats ${computerChoice} you win`;
+    let loseText = `${computerChoice} beats ${humanChoice} you lose`;
+    
+    if(computerChoice == humanChoice){
+        console.log(tieText);
+    }else if(humanChoice == choices[0]){
+        switch(computerChoice){
+            case choices[1]:
+                console.log(loseText);
+                computerScore++;
+                break;
+            case choices[2]:
+                console.log(winText);
+                humanScore++;
+                break;    
+        }
+    } 
+}
+
+playRound(cpu = getComputerChoice(), human = getHumanChoice());
