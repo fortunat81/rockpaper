9a8cf1fa83e510844facaa9e0b4c0098089a3f9d
fortunat81 <163460178+fortunat81@users.noreply.github.com>
2024-07-31 05:29:56 -0400

    complete simple ui exercise

--- script.js
+++ script.js
@@ -1,79 +1,92 @@
-console.log("hello world");
-
 let choices = ["rock", "paper", "scissors"];
 
-function getComputerChoice(){
+function getComputerChoice() {
     return choices[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice(){
-    let choice = "";
-    while(choice != "rock" && choice != "paper" && choice != "scissors"){
-        choice = prompt("rock, paper, scissors");
-        choice = choice.toLowerCase();
-    }
-    return choice;
+function updateScore(scoreToUpdate){
+  scoreToUpdate.textContent = Number(scoreToUpdate.textContent) + 1;
 }
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    let round = 0;
+function updateResultsText(results){
+    resultsText.textContent = results;
+}
 
-    function playRound(computerChoice, humanChoice){
-        let tieText = `${humanChoice} is the same as ${computerChoice} it's a tie`;
-        let winText = `${humanChoice} beats ${computerChoice} you win`;
-        let loseText = `${computerChoice} beats ${humanChoice} you lose`;
-        
-        if(computerChoice == humanChoice){
-            console.log(tieText);
-        }else if(humanChoice == choices[0]){
-            switch(computerChoice){
-                case choices[1]:
-                    console.log(loseText);
-                    computerScore++;
-                    break;
-                case choices[2]:
-                    console.log(winText);
-                    humanScore++;
-                    break;    
-            }
-        }else if(humanChoice == choices[1]){
-            switch(computerChoice){
-                case choices[0]:
-                    console.log(winText);
-                    humanScore++;
-                    break;
-                case choices[2]:
-                    console.log(loseText);
-                    computerScore++;
-                    break;    
-            }
-        } else if(humanChoice == choices[2]){
-            switch(computerChoice){
-                case choices[0]:
-                    console.log(loseText);
-                    computerScore++;
-                    break;
-                case choices[1]:
-                    console.log(winText);
-                    humanScore++;
-                    break;    
-            }
+let gameContainer =document.querySelector("#gameContainer");
+let humanScore = document.querySelector("#humanScore");
+let computerScore = document.querySelector("#cpuScore");
+let resultsText = document.querySelector("#results");
+let round = document.querySelector("#round");
+gameContainer.addEventListener("click", (e) => {
+    playRound(e.target.value, getComputerChoice());
+});
+
+function playRound(computerChoice, humanChoice) {
+    let tieText = `${humanChoice} is the same as ${computerChoice} it's a tie`;
+    let winText = `${humanChoice} beats ${computerChoice} you win`;
+    let loseText = `${computerChoice} beats ${humanChoice} you lose`;
+
+
+    if (computerChoice == humanChoice) {
+        updateResultsText(tieText);
+        updateResultsText(tieText);
+    } else if (humanChoice == choices[0]) {
+        switch (computerChoice) {
+            case choices[1]:
+                updateResultsText(loseText);
+                updateScore(computerScore);
+                break;
+            case choices[2]:
+                updateResultsText(winText);
+                updateScore(humanScore);
+                break;
+        }
+    } else if (humanChoice == choices[1]) {
+        switch (computerChoice) {
+            case choices[0]:
+                updateResultsText(winText);
+                updateScore(humanScore);
+                break;
+            case choices[2]:
+                updateResultsText(loseText);
+                updateScore(computerScore);
+                break;
+        }
+    } else if (humanChoice == choices[2]) {
+        switch (computerChoice) {
+            case choices[0]:
+                updateResultsText(loseText);
+                updateScore(computerScore);
+                break;
+            case choices[1]:
+                updateResultsText(winText);
+                updateScore(humanScore);
+                break;
         }
     }
-
-    while(round < 5){
-        playRound(cpu = getComputerChoice(), human = getHumanChoice());
-        round++;
-    }
-
-    if(humanScore == computerScore){
-        console.log(`You score ${humanScore}, cpu score ${computerScore} it's a tie game.`)
-    }else if(humanScore > computerScore){
-        console.log(`You score ${humanScore}, cpu score ${computerScore} you win the game.`)
-    }else{
-        console.log(`You score ${humanScore}, cpu score ${computerScore} you lose the game.`)
+    round.textContent = Number(round.textContent) + 1;
+    console.log(Number(round.textContent) + 1);
+    if(Number(round.textContent > 5 )){
+        declareWinner();
+        resetGame();
     }
 }
-playGame();
+
+function resetGame(){
+    computerScore.textContent = 0;
+    humanScore.textContent = 0;
+    round.textContent = 1;
+}
+
+function declareWinner() {
+    let human = Number(humanScore.textContent);
+    let cpu = Number(computerScore.textContent);
+    if (human == cpu) {
+        console.log(`You score ${human}, cpu score ${cpu} it's a tie game.`)
+    } else if (human > cpu) {
+        console.log(`You score ${human}, cpu score ${cpu} you win the game.`)
+    } else {
+        console.log(`You score ${human}, cpu score ${cpu} you lose the game.`)
+    }
+    resetGame();
+}
